121732952104c090456f69a927dcdb8f701777e5
Ilona Engblom <ilona.engblom@tuta.io>
2020-10-29 18:13:19 +0200

    Start

--- Player.gd
+++ Player.gd
@@ -1,6 +1,15 @@
 extends KinematicBody2D
 
+var motion = Vector2(0,0) # x, y
+
+const SPEED = 500 # * delta
 
 func _physics_process(delta):
-	pass
-	
+	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+		motion.x = SPEED		
+	else:
+		motion.x = 0
+		
+	move_and_slide(motion)

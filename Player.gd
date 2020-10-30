cc053d6f9c3a23e7c8089694ce313d62097c9309
Ilona Engblom <ilona.engblom@tuta.io>
2020-10-30 14:51:59 +0200

    Animated bunny

--- Player.gd
+++ Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var motion = Vector2(0,0) # x, y
 
-const SPEED = 800 # * delta
+const SPEED = 1000 # * delta
 const GRAVITY = 300 # * delta
 const UP = Vector2(0, -1)
 const JUMP_SPEED = 3000
@@ -12,6 +12,7 @@ func _physics_process(delta):
 	apply_gravity()
 	jump()
 	move()
+	animate()
 	move_and_slide(motion, UP)
 	
 
@@ -23,7 +24,7 @@ func apply_gravity():
 		
 		
 func jump():
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
 		
 
@@ -34,3 +35,16 @@ func move():
 		motion.x = SPEED		
 	else:
 		motion.x = 0
+
+
+func animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x < 0:
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("walk")
+	elif motion.x > 0:
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("walk")
+	else:
+		$AnimatedSprite.play("idle")
